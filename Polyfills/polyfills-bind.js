417a44f21071a96096814cf02c11b7d50d808e19
Shekhar Thakur <122804156+shekharrs@users.noreply.github.com>
2024-04-25 21:22:18 +0530

    updated the polyfills folder

--- Polyfills/polyfills-bind.js
+++ Polyfills/polyfills-bind.js
@@ -1,5 +1,12 @@
 /**
  * Polyfills for bind⚡
+   
+   In JavaScript, the bind() method is used to create a new function with a fixed 
+   this value and some of its arguments pre-specified. When a function is called with 
+   bind(), a new function is returned with the this value bound to the argument passed 
+   to bind() and any additional arguments passed to bind() are pre-specified as the 
+   first arguments of the new function.
+   
  */
 
 const obj = {
