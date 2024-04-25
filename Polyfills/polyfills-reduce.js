b09860ef5e570d7e2c4398740b8c1a3fe04e8ea0
Shekhar Thakur <122804156+shekharrs@users.noreply.github.com>
2024-04-25 13:21:38 +0530

    updated the Polyfills topic

---
+++ Polyfills/polyfills-reduce.js
@@ -0,0 +1,21 @@
+/**
+ * Polyfills for Reduce🔥
+ */
+
+const arr = [1,2,3,4,5,6];
+
+function getSum(acc, cur) {
+    return acc + cur;
+}
+
+Array.prototype.myReduce = function(callback, initalValue){
+    let acc = initalValue ? initalValue : this[0];            // acc = index 0(1);
+    for(let i= initalValue ? 0 : 1; i<this.length ; i++){
+        acc = callback.call(this, acc, this[i], i, this);
+    }
+    return acc;
+}
+
+const total = arr.myReduce(getSum);
+console.log(total);
+
